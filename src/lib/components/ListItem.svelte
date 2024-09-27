0f1881c74daa2e6d7b023e4db052401cb96500be
Matthew Gibbons <mhgibbons@me.com>
2024-09-27 07:11:11 +0000

    Explicit prop type

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
 	import { Node } from '@accuser/svelte-markdown-provider';
+	import type { ListItem } from 'mdast';
 
-	const node: import('mdast').ListItem = $props();
-
-	const { checked, children, spread } = $derived.by(() => ({
-		...node,
-		children:
-			// If the list item contains a single paragraph, unwrap it
-			node.children && node.children.length === 1 && node.children[0].type === 'paragraph'
-				? node.children[0].children
-				: node.children
-	}));
+	let { checked, children, spread }: ListItem = $props();
 </script>
+
+<input checked={true} disabled />
 
 <li>
 	{#if typeof checked === 'boolean'}<input type="checkbox" {checked} disabled />{/if}
 	{#each children as node}<Node {...node} />{/each}
 </li>
-
-<style lang="postcss">
-	li:has(input[type='checkbox']) {
-		@apply list-none;
-
-		& input[type='checkbox'] {
-			@apply align-middle;
-			margin: 0 0.5rem 0 -1.5rem;
-		}
-	}
-</style>
